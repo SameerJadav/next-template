5c23698e6a9df9db89270ad83f11146f661586f9
SameerJadav <sameerjadav010@gmail.com>
2023-07-01 13:42:23 +0530

    feat: Include metadata configuration

---
+++ src/config/site.ts
@@ -0,0 +1,16 @@
+export const siteConfig = {
+  name: "Next Template",
+  description: "Minimal Next.js template.",
+  url: "https://next-template-livid.vercel.app",
+  ogImage: "https://og.sameerjadav.me/og?title=Next%20Template",
+  author: "Sameer Jadav",
+  authorUrl: "https://sameerjadav.me",
+  twitterId: "@SameerJadav_",
+  links: {
+    twitter: "https://twitter.com/SameerJadav_",
+    github: "https://github.com/SameerJadav",
+    linkedin: "https://www.linkedin.com/in/sameer-jadav",
+    instagram: "https://www.instagram.com/sameerjadav._",
+    mail: "mailto:sameerjdav001@gmail.com",
+  },
+}
